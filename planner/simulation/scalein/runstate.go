c3458b0a6ecbf11b39533b2b7cc76a1eb1462378
aaronfern <aaron.francis.fernandes@sap.com>
2026-04-10 14:55:12 +0530

    Add simulation.Run for scale in

--- planner/simulation/scalein/runstate.go
+++ planner/simulation/scalein/runstate.go
@@ -2,12 +2,26 @@ package scalein
 
 import (
 	"context"
-	commontypes "github.com/gardener/scaling-advisor/api/common/types"
-	"github.com/gardener/scaling-advisor/api/minkapi"
-	plannerapi "github.com/gardener/scaling-advisor/api/planner"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/util/sets"
+	"fmt"
+	"maps"
+	"slices"
 	"sync/atomic"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	commontypes "github.com/gardener/scaling-advisor/api/common/types"
+	sacorev1alpha1 "github.com/gardener/scaling-advisor/api/core/v1alpha1"
+	"github.com/gardener/scaling-advisor/api/minkapi"
+	"github.com/gardener/scaling-advisor/api/minkapi/typeinfo"
+	plannerapi "github.com/gardener/scaling-advisor/api/planner"
+	"github.com/gardener/scaling-advisor/common/nodeutil"
+	"github.com/gardener/scaling-advisor/common/objutil"
+	"github.com/gardener/scaling-advisor/common/podutil"
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	eventsv1 "k8s.io/api/events/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/client-go/tools/cache"
 )
 
 // RunState holds internal run state details of parent ScaleInSimulation.
@@ -46,4 +60,232 @@ func FreshRunState() RunState {
 func (r *RunState) Init(parentCtx context.Context, name string, runNum uint32, view minkapi.View) (context.Context, error) {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (r *RunState) GetPodsToReschedule() sets.Set[commontypes.NamespacedName] {
+	return r.leftoverUnscheduledPodNames
+}
+
+func (r *RunState) GetPodNodeAssignments() ([]plannerapi.NodePodAssignment, error) {
+	assignedNodeNames := slices.Collect(maps.Keys(r.scheduledPodNamesByNodeName))
+	assignedNodes, err := r.view.ListNodes(r.ctx, assignedNodeNames...)
+	if err != nil {
+		return nil, err
+	}
+	otherAssignments := make([]plannerapi.NodePodAssignment, len(assignedNodes))
+	for _, node := range assignedNodes {
+		scheduledPodInfos := r.getScheduledPodInfosForNode(node.Name)
+		if len(scheduledPodInfos) > 0 {
+			nodeResources := getNodeResourceInfo(&node)
+			otherAssignments = append(otherAssignments, plannerapi.NodePodAssignment{
+				NodeResources: nodeResources,
+				ScheduledPods: scheduledPodInfos,
+			})
+		}
+	}
+	return otherAssignments, nil
+}
+
+func (s *RunState) getScheduledPodInfosForNode(nodeName string) []plannerapi.PodResourceInfo {
+	scheduledPodNames := s.scheduledPodNamesByNodeName[nodeName].UnsortedList()
+	if len(scheduledPodNames) == 0 {
+		return nil
+	}
+	scheduledPodInfos := make([]plannerapi.PodResourceInfo, 0, len(scheduledPodNames))
+	for _, podName := range scheduledPodNames {
+		scheduledPodInfos = append(scheduledPodInfos, s.unscheduledPods[podName])
+	}
+	return scheduledPodInfos
+}
+
+func getNodeResourceInfo(node *corev1.Node) plannerapi.NodeResourceInfo {
+	instanceType := nodeutil.GetInstanceType(node)
+	return plannerapi.NodeResourceInfo{
+		Name:         node.Name,
+		InstanceType: instanceType,
+		Capacity:     node.Status.Capacity,
+		Allocatable:  node.Status.Allocatable,
+	}
+}
+
+// TODO: take care of volumes attached to this node as well
+func (r *RunState) RemoveNodeAndUnbindPods(nodeName string) ([]commontypes.NamespacedName, error) {
+	log := logr.FromContextOrDiscard(r.ctx)
+
+	podListObj, err := r.view.ListObjects(r.ctx, typeinfo.PodsDescriptor.GVK, minkapi.MatchCriteria{})
+	if err != nil {
+		return nil, err
+	}
+
+	podList, ok := podListObj.(*corev1.PodList)
+	if !ok {
+		return nil, err
+	}
+
+	var unboundPods []commontypes.NamespacedName
+	for _, pod := range podList.Items {
+		if pod.Spec.NodeName != nodeName {
+			continue
+		}
+
+		log.V(2).Info("Unbinding pod from node", "pod", pod.Name, "node", nodeName)
+		pod.Spec.NodeName = ""
+		if err = r.view.UpdateObject(r.ctx, typeinfo.PodsDescriptor.GVK, &pod); err != nil {
+			return nil, err
+		}
+
+		//Remove pod from scheduledPodNamesByNodeName map and to unscheduledPods map in RunState
+		// It has to exist in scheduledPodNamesByNodeName, no? is this check needed?
+		if scheduledPods, exists := r.scheduledPodNamesByNodeName[nodeName]; exists {
+			scheduledPods.Delete(commontypes.NamespacedName{Namespace: pod.Namespace, Name: pod.Name})
+		}
+		r.unscheduledPods[commontypes.NamespacedName{Namespace: pod.Namespace, Name: pod.Name}] = podutil.PodResourceInfoFromCoreV1Pod(&pod)
+
+		unboundPods = append(unboundPods, commontypes.NamespacedName{Namespace: pod.Namespace, Name: pod.Name})
+	}
+
+	// Delete node from view
+	err = r.view.DeleteObject(r.ctx, typeinfo.NodesDescriptor.GVK, cache.NewObjectName("", nodeName))
+	if err != nil {
+		return nil, err
+	}
+	return unboundPods, nil
+}
+
+// Track is used to track the RunState of the simulation by recording the pod-node binding(s) if any made in this
+// [RunState]'s view by the `kube-scheduler`. It returns true if the RunState has not changed over many Track
+// attempts that exceed tbe given maxUnchangedTrackAttempts or an error.
+//
+// Track does the following internally:
+//   - Increments numTrackAttempts and gets the last slice of events (if any) in the [minkapi.EventSink] of
+//     this RunState's [minkapi.View].
+//   - If the slice of events is empty, increment numUnchangedTrackAttempts.
+//     If the numUnchangedTrackAttempts > maxUnchangedTrackAttempts,
+//     then stabilized is considered as true and returned.
+//   - If the slice of event is not empty, reset numUnchangedTrackAttempts and also invoke Reset on the
+//     [minkapi.EventSink]
+//   - For each "Scheduled" event in the slice of events, add the scheduled pod name to
+//     scheduledPodNamesByNodeName, remove scheduled pod name from leftoverUnscheduledPodNames
+func (r *RunState) Track(maxUnchangedTrackAttempts int) (stabilized bool, err error) {
+	log := logr.FromContextOrDiscard(r.ctx)
+	r.numTrackAttempts++
+	evList := r.view.GetEventSink().List()
+	log.V(4).Info("Track Invoked", "numEvents", len(evList),
+		"numTrackAttempts", r.numTrackAttempts,
+		"numUnchangedTrackAttempts", r.numUnchangedTrackAttempts,
+		"maxUnchangedTrackAttempts", maxUnchangedTrackAttempts)
+	if len(evList) == 0 {
+		r.numUnchangedTrackAttempts++
+		if r.numUnchangedTrackAttempts > maxUnchangedTrackAttempts {
+			log.V(3).Info("simulation RunState stabilized - no new kube-scheduler events observed",
+				"numReceivedEvents", r.numReceivedEvents,
+				"maxUnchangedTrackAttempts", maxUnchangedTrackAttempts,
+				"numUnchangedTrackAttempts", r.numUnchangedTrackAttempts,
+				"numScheduledPods", r.numScheduledPods)
+			stabilized = true
+		}
+		return
+	} else if err = r.view.GetEventSink().Reset(); err != nil {
+		r.numUnchangedTrackAttempts = 0
+		return
+	}
+
+	for idx, ev := range evList {
+		var eventTime = ev.EventTime
+		if ev.Series != nil {
+			eventTime = ev.Series.LastObservedTime
+		}
+		log.V(5).Info("Checking event", "index", idx, "id", ev.UID, "eventTime", eventTime,
+			"ReportingController", ev.ReportingController, "ReportingInstance", ev.ReportingInstance,
+			"Action", ev.Action, "Reason", ev.Reason, "Regarding", ev.Regarding, "Note", ev.Note)
+		r.numReceivedEvents++
+		if ev.Action != "Binding" && ev.Reason != "Scheduled" {
+			if ev.Reason == "FailedScheduling" {
+				log.V(4).Info("FailedScheduling event", "index", idx, "id", ev.UID,
+					"ReportingController", ev.ReportingController, "ReportingInstance", ev.ReportingInstance,
+					"Action", ev.Action, "Reason", ev.Reason, "Regarding", ev.Regarding, "Note", ev.Note)
+			}
+			continue
+		}
+		if err = r.handleScheduledPodEvent(ev); err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (r *RunState) handleScheduledPodEvent(ev eventsv1.Event) error {
+	log := logr.FromContextOrDiscard(r.ctx)
+	podNsName := objutil.NamespacedNameFromEventRegarding(ev)
+	log.V(4).Info("PodScheduled event.", "podNamespacedName", podNsName, "eventNote", ev.Note)
+	obj, err := r.view.GetObject(r.ctx, typeinfo.PodsDescriptor.GVK, podNsName.AsObjectName())
+	if err != nil {
+		return err
+	}
+	pod, ok := obj.(*corev1.Pod)
+	if !ok {
+		return fmt.Errorf("object %T and name %q is not a Pod", pod, podNsName)
+	}
+	if pod.Spec.NodeName == "" {
+		return fmt.Errorf("scheduledPod %q has no assigned node name even with binding event note %q", podNsName, ev.Note)
+	}
+	err = r.addScheduledPod(pod)
+	return err
+}
+
+func (r *RunState) addScheduledPod(pod *corev1.Pod) error {
+	log := logr.FromContextOrDiscard(r.ctx)
+	podNsName := objutil.NamespacedName(pod)
+	if pod.Spec.NodeName == "" {
+		return fmt.Errorf("nodeName must be assigned to pod %q", podNsName)
+	}
+	scheduledPodNames := r.scheduledPodNamesByNodeName[pod.Spec.NodeName]
+	if scheduledPodNames == nil {
+		scheduledPodNames = sets.New[commontypes.NamespacedName]()
+	}
+	scheduledPodNames.Insert(podNsName)
+	r.scheduledPodNamesByNodeName[pod.Spec.NodeName] = scheduledPodNames
+	r.numScheduledPods++
+	r.leftoverUnscheduledPodNames.Delete(podNsName)
+	r.numUnchangedTrackAttempts = 0
+	log.V(4).Info("Added scheduledPod to RunState.scheduledPodNamesByNodeName and reset numUnchangedTrackAttempts",
+		"podNamespacedName", podNsName,
+		"numScheduledPods", r.numScheduledPods,
+		"leftoverUnscheduledPodCount", len(r.leftoverUnscheduledPodNames))
+	return nil
+}
+
+func (r *RunState) NodePodAssignments(unboundPods []commontypes.NamespacedName) ([]plannerapi.NodePodAssignment, error) {
+	var nodePodAssignments []plannerapi.NodePodAssignment
+	for _, podNN := range unboundPods {
+		podObj, err := r.view.GetObject(r.ctx, typeinfo.PodsDescriptor.GVK, cache.NewObjectName(podNN.Namespace, podNN.Name))
+		if err != nil {
+			return nil, err
+		}
+		pod, ok := podObj.(*corev1.Pod)
+		if !ok {
+			return nil, err
+		}
+		if pod.Spec.NodeName == "" {
+			continue
+		}
+		nodePodAssignments = append(nodePodAssignments, plannerapi.NodePodAssignment{
+			NodeResources: getNodeResourceInfo(&corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: pod.Spec.NodeName}}),
+			ScheduledPods: []plannerapi.PodResourceInfo{podutil.PodResourceInfoFromCoreV1Pod(pod)},
+		})
+	}
+	return nodePodAssignments, nil
+}
+
+func (r *RunState) GetScaleInItems() []sacorev1alpha1.ScaleInItem {
+	scaleInItems := make([]sacorev1alpha1.ScaleInItem, 0, len(r.scaleInNodes))
+	for _, node := range r.scaleInNodes {
+		scaleInItems = append(scaleInItems, sacorev1alpha1.ScaleInItem{
+			//TODO: add nodeplacement
+			//NodePlacement: np,
+			NodeName: node.Name,
+		})
+	}
+	return scaleInItems
 }
